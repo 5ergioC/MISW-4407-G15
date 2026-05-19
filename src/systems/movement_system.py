82cf37520e6f64857e1971d426ed262346159f2f
DavidCepeda13 <j.cepedab@uniandes.edu.co>
2026-05-18 21:43:52 -0500

    Landers to Mutants

--- src/systems/movement_system.py
+++ src/systems/movement_system.py
@@ -2,6 +2,7 @@ from src.components.player import Player
 from src.components.transform import Transform
 from src.components.velocity import Velocity
 from src.components.enemy import Enemy
+from src.components.state import State
 
 
 # Do not allow enemies to move above the HUD area (top HUD height in pixels)
@@ -15,7 +16,14 @@ class MovementSystem:
             if world.has_component(entity, Player):
                 continue
             transform.position += velocity.value * dt
-            # prevent enemies from going above the HUD area
+            # prevent enemies from going above the HUD area, except when
+            # a lander is actively ascending to mutate (allow it to pass)
             if world.has_component(entity, Enemy):
+                # allow enemies in ascending/transform_to_mutant state to pass the HUD
+                if world.has_component(entity, State):
+                    state = world.component_for_entity(entity, State)
+                    if state.name in {"ascending", "transform_to_mutant"}:
+                        # do not clamp while ascending/transforming
+                        continue
                 if transform.position.y < HUD_BOTTOM_Y:
                     transform.position.y = HUD_BOTTOM_Y
